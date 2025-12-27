9760ad235dc8950e2da5f203f5da55ad3d1fa172
William Lee <lee.iii.will@gmail.com>
2025-12-27 14:47:04 -0500

    feat: add logging to power_service

--- smartmirrord/services/power_service.py
+++ smartmirrord/services/power_service.py
@@ -1,6 +1,9 @@
 import threading
+import logging
 from typing import Callable, Optional
 from smartmirrord.hardware.power_status import PowerStatus
+
+log = logging.getLogger(__name__)
 
 
 class PowerService:
@@ -19,33 +22,55 @@ class PowerService:
         self._stability_timer: Optional[threading.Timer] = None
         self._lock = threading.Lock()
 
+        log.info("PowerService starting")
+
         self._power_gpio = PowerStatus(on_change=self._handle_power_change)
 
         # Read current GPIO level once and start stability timer.
         initial_state = self._power_gpio.read()
-        self._handle_power_change(initial_state)
+        log.info("Initial power GPIO read: %s", "ON" if initial_state else "OFF")
 
-    def _handle_power_change(self, is_on: bool):
+        self._start_stability_timer(initial_state)
+
+    def _start_stability_timer(self, is_on: bool):
         with self._lock:
             self._last_gpio_value = is_on
 
-            # Cancel any existing stability timer
             if self._stability_timer:
                 self._stability_timer.cancel()
+                log.debug("Cancelled previous stability timer")
 
-            # Start a new timer to fire after STABILITY_WINDOW
+            log.debug(
+                "Starting stability timer (%.1fs) for state=%s",
+                self.STABILITY_WINDOW,
+                "ON" if is_on else "OFF",
+            )
+
             self._stability_timer = threading.Timer(
                 self.STABILITY_WINDOW, self._stable_callback, args=(is_on,)
             )
             self._stability_timer.daemon = True
             self._stability_timer.start()
 
+    def _handle_power_change(self, is_on: bool):
+        log.debug("GPIO edge detected: %s", "ON" if is_on else "OFF")
+        self._start_stability_timer(is_on)
+
     def _stable_callback(self, stable_value: bool):
         with self._lock:
             if self._is_on == stable_value:
+                log.debug(
+                    "Stability timer fired but state unchanged (%s)",
+                    "ON" if stable_value else "OFF",
+                )
                 return
 
             self._is_on = stable_value
+
+        log.info(
+            "Power state stabilized: %s",
+            "ON" if stable_value else "OFF",
+        )
 
         try:
             if stable_value and self.on_power_on:
@@ -53,7 +78,7 @@ class PowerService:
             elif not stable_value and self.on_power_off:
                 self.on_power_off()
         except Exception:
-            pass
+            log.exception("Exception in power state callback")
 
     def is_power_on(self) -> bool:
         with self._lock:
